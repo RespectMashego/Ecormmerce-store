3522cdd81b040722e1f6145ce87dfd84a33258af
Respect <r.mashego333@gmail.com>
2023-11-16 19:13:46 +0200

    ffr

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("f9887625-190f-4ebb-9363-65bf662e62f4");
 
   return (
     <Container>
