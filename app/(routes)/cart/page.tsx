aaae1083d83bebe0f307132e8e1a0d760e37d99c
Respect <r.mashego333@gmail.com>
2023-11-17 02:41:57 +0200

    c

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -9,6 +9,7 @@ import Summary from './components/summary'
 import CartItem from './components/cart-item';
 
 export const revalidate = 0;
+export const runtime = "edge"
 
 const CartPage = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -31,7 +32,7 @@ const CartPage = () => {
             <div className="lg:col-span-7">
               {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
               <ul>
-                {cart.items.map((item) => (
+                {cart?.items?.map((item) => (
                   <CartItem key={item.id} data={item} />
                 ))}
               </ul>
